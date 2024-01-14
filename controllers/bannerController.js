adba598149d41a01df4af38ef97498fd82d9bd38
Baivab Bidari <baivabbidari987@gmail.com>
2024-01-14 22:27:48 +0545

    feat:banner section complted

--- controllers/bannerController.js
+++ controllers/bannerController.js
@@ -1,13 +1,41 @@
 const Banner = require("../modals/banner.modal");
 
+// const findOrCreateBanner = async () => {
+//   let existingBanner = await Banner.findOne();
+
+//   if (!existingBanner) {
+//     // If no banner exists, create a new one
+//     existingBanner = await new Banner();
+//     await existingBanner.save();
+//   }
+
+//   return existingBanner;
+// };
+
 exports.createBanner = async (req, res, next) => {
   try {
-    console.log("hitted", req.file);
+    console.log("hitted", req.files);
 
+    let banner = await Banner.findOne();
+
+    if (!banner) {
+      // If no banner exists, create a new one
+      banner = new Banner();
+    }
+
+    // Get image filenames from the uploaded files
     const fileNames = req.files.map((file) => file.filename);
 
-    const bannerImage = new Banner({ bannerImage: fileNames });
-    const datas = await bannerImage.save();
+    // Add image filenames to the banner's bannerImages array
+    banner.bannerImage = [...(banner.bannerImage || []), ...fileNames];
+
+    // Save the banner
+    const datas = await banner.save();
+
+    // const fileNames = req.files.map((file) => file.filename);
+
+    // const bannerImage = new Banner({ bannerImage: fileNames });
+    // const datas = await bannerImage.save();
 
     res.status(201).json({
       message: "success fully creaete banner",
@@ -19,4 +47,31 @@ exports.createBanner = async (req, res, next) => {
 exports.getAllBanner = async (req, res, next) => {
   const data = await Banner.find();
   res.status(201).json({ message: "success fully get Banner", data: data });
+};
+
+exports.deleteBannerImage = async (req, res, next) => {
+  try {
+    const { imageName } = req.params;
+
+    // Find the banner
+    const banner = await Banner.findOne();
+
+    if (!banner) {
+      return res.status(404).send("Banner not found");
+    }
+
+    // Remove the specified imageName from the banner's bannerImages array
+    banner.bannerImage = banner.bannerImage.filter(
+      (image) => image !== imageName
+    );
+
+    // Save the updated banner
+    const datas = await banner.save();
+
+    res
+      .status(201)
+      .json({ message: "successfully deleted Banner", data: datas });
+  } catch (error) {
+    res.status(201).json({ message: "error occured" });
+  }
 };
--- routes/bannerRoutes.js
+++ routes/bannerRoutes.js
@@ -3,7 +3,7 @@ const {
   createBanner,
   getAllBanner,
   updateBanner,
-  deleteBanner,
+  deleteBannerImage,
 } = require("../controllers/bannerController");
 const express = require("express");
 
@@ -29,7 +29,7 @@ const router = express.Router();
 
 router.post("/banner/new", cpUpload, createBanner);
 router.get("/banner", getAllBanner);
-// router.patch("/banner/:bannerId", cpUpload, updateBanner);
-// router.delete("/banner/:bannerId", deleteBanner);
+
+router.delete("/banner/:imageName", deleteBannerImage);
 
 module.exports = router;
