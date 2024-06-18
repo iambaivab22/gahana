5d20f80fc869c30d6c5f0bb4affd2c6cd115452b
baivab <bidaribaivab7@gmail.com>
2024-06-18 22:57:45 +0545

    db added to production

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.DB_LOCAL_URI, {
-    // useNewUrlParser: true,
-    // useCreateIndex: true,
-    // useFindAndModify: false,
-    // useUnifiedTopology: true,
-  });
+  const conn = await mongoose.connect(
+    "mongodb+srv://bidaribaivab7:(meropassword12)@cluster0.l1lbpsa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    {
+      // useNewUrlParser: true,
+      // useCreateIndex: true,
+      // useFindAndModify: false,
+      // useUnifiedTopology: true,
+    }
+  );
   console.log(`MongoDB Connected: ${conn.connection.host}`);
 };
 
