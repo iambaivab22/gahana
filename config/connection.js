b5b259c92bfe8ffac7688503a4924152ac3013da
baivab <bidaribaivab7@gmail.com>
2024-06-22 16:20:15 +0545

    fix:mongo url

--- config/connection.js
+++ config/connection.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   const conn = await mongoose.connect(
-    "mongodb+srv://bidaribaivab7:(meropassword12)@cluster0.l1lbpsa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    // "mongodb://127.0.0.1:27017/ecommerce",
+    // "mongodb+srv://bidaribaivab7:(meropassword12)@cluster0.l1lbpsa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb://127.0.0.1:27017/ecommerce",
     {
       // useNewUrlParser: true,
       // useCreateIndex: true,
