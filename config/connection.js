d7a39cceb2543e76fc398c4765f9ffb057e0cf21
baivab <bidaribaivab7@gmail.com>
2024-06-22 15:53:51 +0545

    db changed

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   const conn = await mongoose.connect(
-    "mongodb+srv://bidaribaivab7:(meropassword12)@cluster0.l1lbpsa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    // "mongodb+srv://bidaribaivab7:(meropassword12)@cluster0.l1lbpsa.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb://127.0.0.1:27017/ecommerce",
     {
       // useNewUrlParser: true,
       // useCreateIndex: true,
